a8414063f19b9200ebe989c9582884af5d3ead43
Tyler Buchea <tyler@buchea.com>
2017-06-07 09:21:45 -0700

    Add styling

--- site/pages/index.js
+++ site/pages/index.js
@@ -7,7 +7,49 @@ export default class App extends Component {
     title: 'Your site',
   };
 
+  style = () =>
+    <style jsx>{`
+      .header {
+        text-align: center;
+      }
+    `}</style>;
+
   render() {
-    return <div>Welcome to {this.state.title}!</div>;
+    return (
+      <div className="site">
+
+        {this.style()}
+
+        <Head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Website Now!</title>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+            integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
+            crossorigin="anonymous"
+          />
+        </Head>
+
+        <div className="container">
+
+          <div className="header pure-u-1">
+            <h1>Welcome to {this.state.title}!</h1>
+            <button className="button-xlarge pure-button-primary">
+              Extra Large Button
+            </button>
+          </div>
+
+          <div className="pure-g">
+            <div className="pure-u-1-3"><p>Thirds</p></div>
+            <div className="pure-u-1-3"><p>Thirds</p></div>
+            <div className="pure-u-1-3"><p>Thirds</p></div>
+          </div>
+
+        </div>
+
+      </div>
+    );
   }
 }
