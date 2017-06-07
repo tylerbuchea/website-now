c558e25dad04a9350ace36060819ab632c8f3524
Tyler Buchea <tyler@buchea.com>
2017-06-06 17:28:21 -0700

    Add pure.css and prettier precommit hook

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,17 +1,13 @@
-import React, {
-  Component,
-} from 'react';
+import React, { Component } from 'react';
+
+import Head from 'next/head';
 
 export default class App extends Component {
-
   state = {
     title: 'Your site',
-  }
+  };
 
   render() {
-    return (
-      <div>Welcome to {this.state.title}!</div>
-    );
+    return <div>Welcome to {this.state.title}!</div>;
   }
-
 }
